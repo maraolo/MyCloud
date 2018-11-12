43acbdb0f4a4aed4c41f8b65bd10fb6328e87e1e
Paolo <paolo@epost.ch>
2018-11-12 16:04:49 +0100

    name

--- modules/EC2/vars.tf
+++ modules/EC2/vars.tf
@@ -1,6 +1,6 @@
 variable "EC2name" {
   description = "The name of the EC2 instance"
-  default = "Paolo"
+  default = ""
 }
 variable "ami" {
   description = "The ID of the AMI that has Ruby on Rails installed and contains the code from the example-rails-app"
