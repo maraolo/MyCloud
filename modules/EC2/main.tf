fd303985e4ab12169d872530d16187e192e8e058
Paolo <paolo@epost.ch>
2018-11-12 20:31:15 +0100

    more variables added

--- modules/EC2/main.tf
+++ modules/EC2/main.tf
@@ -1,12 +1,13 @@
 resource "aws_instance" "EC2Instance" {
   // ...
   ami = "${var.ami}"
-  instance_type = "t2.micro"
+  instance_type = "${var.In_Type}"
+
   # security_groups = ["${aws_security_group.example_rails_app.name}"]
   # user_data = "${data.template_file.user_data.rendered}"
   # key_name = "${var.key_pair_name}"
   
   tags {
-    Name = "${var.EC2name}"
+    Name = "${var.EC2Name}"
   }
 }
